931e9c66f4576db9e22c1335c5f5d90c4b398a44
Garrick.Ho <hogarrick@gmail.com>
2025-06-27 14:46:31 +0800

    .

---
+++ src/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install VS Code
+# Ref: https://www.geeksforgeeks.org/install-visual-studio-code-in-kali-linux/
+sudo apt update
+#sudo apt install curl gpg gnupg2 software-properties-common apt-transport-https
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update && sudo apt install code
